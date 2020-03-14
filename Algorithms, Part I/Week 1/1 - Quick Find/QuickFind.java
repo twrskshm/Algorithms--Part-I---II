a2ffc32f0b27c91bb44737f65130b050c7f73a06
Saksham Tiwari <contact@tsaksham.com>
2020-03-14 02:58:10 -0400

    Quick Find commit
    Quick Find commit

--- Algorithms, Part I/Week 1/1 - Quick Find/QuickFind.java
+++ Algorithms, Part I/Week 1/1 - Quick Find/QuickFind.java
@@ -1,26 +1,25 @@
+package com.company;
+
 public class QuickFind {
-    private int array[];
+    private int[] array;
 
     public QuickFind(int n) {
         array = new int[n];
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++)
             array[i] = i;
-        }
     }
 
-    public void union(int node_one, int node_two) {
-        int node_one_id = array[node_one];
-        int node_two_id = array[node_two];
-
-        for(int i = 0; i < array.length; i++) {
-            if(array[i] == node_one_id) {
-                array[i] = array[node_two_id];
-            }
-        }
+    public boolean connected(int index_one, int index_two) {
+        return array[index_one] == array[index_two];
     }
 
-    public boolean connected(int node_one, int node_two) {
-        return array[node_one] == array[node_two];
+    public void union(int index_one, int index_two) {
+        int node_one_parent_id = array[index_one];
+        int node_two_parent_id = array[index_two];
+
+        for(int i = 0; i < array.length; i++)
+            if(array[i] == node_two_parent_id)
+                array[i] = node_one_parent_id;
     }
 }
