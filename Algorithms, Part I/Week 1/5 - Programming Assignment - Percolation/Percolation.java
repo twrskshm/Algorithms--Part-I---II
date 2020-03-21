82f5249c8cda2f662fe900c4a9cc51be089143a0
Saksham Tiwari <contact@tsaksham.com>
2020-03-20 20:58:30 -0400

    Percolation update
    Percolation update

--- Algorithms, Part I/Week 1/5 - Programming Assignment - Percolation/Percolation.java
+++ Algorithms, Part I/Week 1/5 - Programming Assignment - Percolation/Percolation.java
@@ -71,13 +71,13 @@ public class Percolation {
         return weighted_quick_union.connected(VIRTUAL_TOP_INDEX, VIRTUAL_BOTTOM_INDEX);
     }
 
-    public void validate(int row, int col) {
-        if(row < 1 || row > N || col < 1 || col > N)
+    public void validate(int n) {
+        if(n < 1)
             throw new IllegalArgumentException();
     }
 
-    public void validate(int n) {
-        if(n < 1)
+    public void validate(int row, int col) {
+        if(row < 1 || row > N || col < 1 || col > N)
             throw new IllegalArgumentException();
     }
 
